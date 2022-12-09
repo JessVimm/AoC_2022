6621a9117743b60d57af18c606e7fbbbef146c10
Jess Vimm <jessvimm@gmail.com>
2022-12-08 23:21:09 -0600

    first commit

---
+++ Day1/day01.js
@@ -0,0 +1,35 @@
+const getAoCInput =  async() => {
+    const response = await fetch("https://adventofcode.com/2022/day/1/input") 
+    const data = await response.text();
+    return responseText;
+}; 
+
+const puzzleData = getAoCInput();   
+
+const addReducer = (sum, cal) => sum + cal
+
+const getSumOfCal = (group) =>
+    group
+    .split("\n")
+    .map(Number) 
+    .reduce(addReducer, 0);
+
+// Get each elfs calories by groups
+const caloriesByElf = puzzleData.split("\n\n");
+
+// Find the sum of each group
+const eachElfsCalories = caloriesByElf.map(getSumOfCal);
+
+// Get the highest
+const maxCalSum = Math.max(...eachElfsCalories);
+
+// Get three highest
+// Copy the sum of calories by elf, sort them in desc order
+const sortedElfsCalories = [...eachElfsCalories].sort((val1, val2) => val2 - val1); 
+
+// Take top 3 and add them
+const top3 = sortedElfsCalories.slice(0, 3);
+const top3Sum = top3.reduce(addReducer, 0); 
+
+// Input result
+console.log(top3Sum);
